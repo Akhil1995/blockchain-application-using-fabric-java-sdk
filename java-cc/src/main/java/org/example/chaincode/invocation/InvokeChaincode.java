628d750674fb4a79fcd0dd9883ed11117c201b37
Akhil1995 <akhilsai2712@gmail.com>
2020-04-23 01:10:28 -0700

    removed user authentication

--- java-cc/src/main/java/org/example/chaincode/invocation/InvokeChaincode.java
+++ java-cc/src/main/java/org/example/chaincode/invocation/InvokeChaincode.java
@@ -80,11 +80,10 @@ public class InvokeChaincode {
 			TransactionProposalRequest request = fabClient.getInstance().newTransactionProposalRequest();
 			ChaincodeID ccid = ChaincodeID.newBuilder().setName(ccName).build();
 			User usercontext = Util.readUserContext(Config.ORG1, sArgs[0]);
-			//if(usercontext==null) {
-				//Logger.getLogger(InvokeChaincode.class.getName()).log(Level.SEVERE,"User not registered");
-				//return;
-			//}
-			request.setUserContext(usercontext);
+			if(usercontext!=null) {
+				Logger.getLogger(InvokeChaincode.class.getName()).log(Level.SEVERE,"User not registered");
+				request.setUserContext(usercontext);
+			}
 			request.setChaincodeID(ccid);
 			request.setFcn(fcnName);
 			//String[] arguments = { "CAR1", "Chevy", "Volt", "Red", "Nick" };
