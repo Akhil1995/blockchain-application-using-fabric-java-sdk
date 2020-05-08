7fd72bbbf36d233f8fbe2f8814d00c9d25d55b79
Akhil1995 <akhilsai2712@gmail.com>
2020-05-08 02:29:41 -0700

    debugging

--- java-cc/src/main/java/org/example/chaincode/invocation/QueryChaincode.java
+++ java-cc/src/main/java/org/example/chaincode/invocation/QueryChaincode.java
@@ -160,7 +160,6 @@ public class QueryChaincode {
 								// big problem here is we need to know all the values that were read in a transaction
 								rwset.getRwset().getReadsList().forEach(read->{
 									// if this is not the first read
-									System.out.println(read);
 									if(read.getVersion().getBlockNum() > 0L) {
 										TxnRead txnRead = new TxnRead(read.getKey(),read.getVersion().getBlockNum());
 										// if the value is already present in the cache, re use it
@@ -360,6 +359,7 @@ public class QueryChaincode {
 				keySet.add(write.getKey());
 			});
 			long blockNumber = first_txn.getBlockHeight();
+			System.out.println(sortedMap);
 //			while(!keyQueue.isEmpty()) {
 //				String key = keyQueue.poll();
 //				sArgs[1] = key;
